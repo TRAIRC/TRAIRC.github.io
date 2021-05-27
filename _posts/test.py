6cffe6c66ed38dd60e251dce87e7b477309c5e60
txwfj <46244590+txwfj@users.noreply.github.com>
2021-05-27 15:29:02 +0800

    ADD

--- _posts/test.py
+++ _posts/test.py
@@ -115,10 +115,79 @@ def create_markdown_file_with_number(f,file_name):
             print('文件已生成')
     else:
         print('文件名重复，请修改文件'+ markdown_file_with_number +'的文件名后重试')
+import sys
+import os
+import time
 
-file_name = ''
-#如果未传入文件
-if len(sys.argv) < 2:
+headline = ['#','##','###','####','#####','######']
+lines_in_file = []
+
+"""生成目录列表中的某一项"""
+def creat_directory_line(line,headline_mark,i):
+    if headline_mark == '#':
+        return '<a href="#' + str(i) + '">' + line[2:-1] + "</a>  \n"
+    elif headline_mark == '##':
+        #&emsp;为Markdown中的一种缩进，这里不直接用空格作为缩进是因为多个空格一起出现可能会生成代码块，引发歧义
+        return '&emsp;<a href="#' + str(i) + '">' + line[3:-1] + "</a>  \n"
+    elif headline_mark == '###':
+        return '&emsp;&emsp;<a href="#' + str(i) + '">' + line[4:-1] + "</a>  \n"
+    elif headline_mark == '####':
+        return '&emsp;&emsp;&emsp;<a href="#' + str(i) + '">' + line[5:-1] + "</a>  \n"
+    elif headline_mark == '#####':
+        return '&emsp;&emsp;&emsp;&emsp;<a href="#' + str(i) + '">' + line[6:-1] + "</a>  \n"
+    elif headline_mark == '######':
+        return '&emsp;&emsp;&emsp;&emsp;&emsp;<a href="#' + str(i) + '">' + line[7:-1] + "</a>  \n"
+
+"""生成目录列表"""
+def creat_directory(f):
+    i = 0
+    directory = []
+    directory.append('<a name="index">**Index**</a>\n')
+    for line in f:
+        lines_in_file.append(line)
+    f.close()
+    length = len(lines_in_file)
+    for j in range(length):
+        splitedline = lines_in_file[j].lstrip().split(' ')
+        if splitedline[0] in headline:
+            #如果为最后一行且末尾无换行（防最后一个字被去除）
+            if j == length - 1 and lines_in_file[j][-1] != '\n':
+                directory.append(creat_directory_line(lines_in_file[j] + '\n',splitedline[0],i) + '\n')
+                lines_in_file[j] = lines_in_file[j].replace(splitedline[0] + ' ',splitedline[0] + ' ' + '<a name="' + str(i) + '">')[:] + '</a><a style="float:right;text-decoration:none;" href="#index">[Top]</a>' + "\n"
+                i = i + 1
+            else:
+                directory.append(creat_directory_line(lines_in_file[j],splitedline[0],i))
+                lines_in_file[j] = lines_in_file[j].replace(splitedline[0] + ' ',splitedline[0] + ' ' + '<a name="' + str(i) + '">')[:-1] + '</a><a style="float:right;text-decoration:none;" href="#index">[Top]</a>' + "\n"
+                i = i + 1
+    return directory
+
+"""以目录列表为参数生成添加目录的文件"""
+def creat_file_with_toc(f,file_name):
+    directory = creat_directory(f)
+    lines_in_file = []
+    lines_in_file_with_number = []
+    f=open(file_name,'r',encoding='utf-8')
+    lines_in_file = f.readlines()
+    #print(lines_in_file)
+    f.close()
+    lines_in_file_with_number = create_lines_with_number(lines_in_file)
+    # 根据原文件名生成标题添加了序号的文件的文件名
+    markdown_file_with_number = os.getcwd() + '\\' + file_name[::-1].split('.', 1)[1][::-1] + '_withNum.md'
+    if not os.path.exists(markdown_file_with_number):
+        with open(markdown_file_with_number, 'w+',encoding='utf-8') as f:
+            for directory_line in directory:
+                f.write(directory_line)
+            for line in lines_in_file_with_number:
+               # print(line)
+                f.write(line)
+            print('文件已生成')
+    else:
+        print('文件名重复，请修改文件'+'file_with_toc.md'+'的文件名后重试')
+
+if __name__=='__main__':
+    file_name = ''
+    #如果未传入文件名
+    if len(sys.argv) < 2:
         path = os.getcwd()
         file_and_dir = os.listdir(path)
         md_file = []
@@ -134,11 +203,12 @@ if len(sys.argv) < 2:
             print('该目录下无Markdown文件，即将退出...')
             time.sleep(2)
             os._exit(0)
-else:
-    file_name = sys.argv[1]
-if os.path.exists(file_name):
-    with open(file_name,'r',encoding='utf-8') as f:
-        create_markdown_file_with_number(f,file_name)
-else:
-    msg = "未找到文件"
-    print(msg)
+    else:
+        file_name = sys.argv[1]
+    if os.path.exists(file_name) and os.path.isfile(file_name):
+        with open(file_name,'r',encoding='utf-8') as f:
+            creat_file_with_toc(f,file_name)
+
+    else:
+        msg = "未找到文件"
+        print(msg)
